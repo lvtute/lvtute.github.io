c7c836dc77081dc6c1e5860446b0d9e41ba72d31
shelbyyay <129026105+shelbyyay@users.noreply.github.com>
2024-04-28 14:51:08 +0700

    Add first md file reader

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+
+import Markdown from 'markdown-to-jsx'
 
 function App() {
 
+  const fileName = "test"
+  const [post, setPost ] = useState<string>('')
+
+
+  useEffect(() => {
+    import(`./markdowns/${fileName}.md`)
+    .then(res => {
+      fetch(res.default)
+      .then(response => response.text())
+      .then(text => setPost(text))
+  })
+  }, []);
+  
   return (
-  <>Hello worldz</>
+    <Markdown >{post}</Markdown>
   )
 }
 
