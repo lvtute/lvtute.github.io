760eef60970aca0ff6e72b52319154cb8de9eda1
shelbyyay <129026105+shelbyyay@users.noreply.github.com>
2024-04-29 14:28:18 +0700

    Clean up project

---
+++ src/App-backup.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+
+import Markdown from 'markdown-to-jsx';
+import pdf from './Resume.pdf';
+
+function App() {
+  const fileName = 'test';
+  const [post, setPost] = useState<string>('');
+
+  useEffect(() => {
+    import(`./markdowns/${fileName}.md`).then((res) => {
+      fetch(res.default)
+        .then((response) => response.text())
+        .then((text) => setPost(text));
+    });
+  }, []);
+
+  const openResumePdfInNewTab = () => {
+    window.open(pdf, '_blank');
+  };
+
+  return (
+    <>
+      <Markdown>{post}</Markdown>
+      <button onClick={openResumePdfInNewTab}>pdf</button>
+    </>
+  );
+}
+
+export default App;
--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,5 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-
-import Markdown from 'markdown-to-jsx';
-import pdf from './Resume.pdf';
-
-function App() {
-  const fileName = 'test';
-  const [post, setPost] = useState<string>('');
-
-  useEffect(() => {
-    import(`./markdowns/${fileName}.md`).then((res) => {
-      fetch(res.default)
-        .then((response) => response.text())
-        .then((text) => setPost(text));
-    });
-  }, []);
-
-  const openResumePdfInNewTab = () => {
-    window.open(pdf, '_blank');
-  };
-
-  return (
-    <>
-      <Markdown>{post}</Markdown>
-      <button onClick={openResumePdfInNewTab}>pdf</button>
-    </>
-  );
-}
+const App = () => {
+  return <>Hello world</>;
+};
 
 export default App;
