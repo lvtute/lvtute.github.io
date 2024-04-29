3bba65fa5b6d09b927f6afcf6c9e79f90bc54ad7
shelbyyay <129026105+shelbyyay@users.noreply.github.com>
2024-04-29 13:56:18 +0700

    Config prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
-import Markdown from 'markdown-to-jsx'
-import pdf from './Resume_LuongVanThuan_JrDev.pdf'
+import Markdown from 'markdown-to-jsx';
+import pdf from './Resume_LuongVanThuan_JrDev.pdf';
 
 function App() {
-
-  const fileName = "test"
-  const [post, setPost ] = useState<string>('')
-
+  const fileName = 'test';
+  const [post, setPost] = useState<string>('');
 
   useEffect(() => {
-    import(`./markdowns/${fileName}.md`)
-    .then(res => {
+    import(`./markdowns/${fileName}.md`).then((res) => {
       fetch(res.default)
-      .then(response => response.text())
-      .then(text => setPost(text))
-  })
+        .then((response) => response.text())
+        .then((text) => setPost(text));
+    });
   }, []);
 
   const openPdf = () => {
-    window.open(pdf, '_blank')
-  }
-  
-  
+    window.open(pdf, '_blank');
+  };
+
   return (
     <>
-    <Markdown >{post}</Markdown>
-    <button onClick={openPdf}>pdf</button>
+      <Markdown>{post}</Markdown>
+      <button onClick={openPdf}>pdf</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
