e76e78506758fee8e51c4c41c07ff011f7a397d5
Thuan <lvtute@gmail.com>
2024-10-22 20:30:58 +0700

    Init UI template

--- src/hooks/useBodyBackgroundColor.ts
+++
@@ -1,9 +0,0 @@
-import { CSSProperties, useLayoutEffect } from 'react';
-
-const useBodyBackgroundColor = (color: CSSProperties['backgroundColor']) => {
-  useLayoutEffect(() => {
-    document.body.style.backgroundColor = color?.toString() ?? 'white';
-  });
-};
-
-export default useBodyBackgroundColor;
