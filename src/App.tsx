d7104e73e98ff7725ec452f0bc53d6a3f0a3489e
Thuan <lvtute@gmail.com>
2024-11-05 18:53:40 +0700

    Back to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import PageContainer from './components/PageContainer';
 import About from './pages/About';
 import CodingNotes from './pages/CodingNotes';
 import Projects from './pages/Projects';
 
 const App = () => {
-  console.log('Hello world');
   return (
     <PageContainer>
-      <BrowserRouter basename="/">
+      <HashRouter basename="/">
         <Routes>
           <Route path="/" element={<About />} />
           <Route path="coding-notes" element={<CodingNotes />} />
           <Route path="projects" element={<Projects />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </PageContainer>
   );
 };
