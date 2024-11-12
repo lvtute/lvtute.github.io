79dbf84f45165a73236517d40ac23fdd1bc8773f
Thuan <lvtute@gmail.com>
2024-11-12 20:52:50 +0700

    Fix markdown

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -8,9 +8,9 @@ import Markdown from 'markdown-to-jsx';
 const posts = _posts as unknown as Post[];
 console.log({ mode: import.meta.env.MODE });
 
-const DEV_MARKDOWNS_PATH = '/src/markdowns';
+const DEV_MARKDOWNS_PATH = '/src/assets/markdowns';
 const PROD_MARKDOWNS_PATH =
-  'https://raw.githubusercontent.com/lvtute/lvtute.github.io/refs/heads/main/src/markdowns';
+  'https://raw.githubusercontent.com/lvtute/lvtute.github.io/refs/heads/main/src/assets/markdowns';
 
 const MARKDOWNS_PATH =
   import.meta.env.MODE === 'development'
@@ -24,15 +24,28 @@ const Post = () => {
 
   const [content, setContent] = useState<string | null>(null);
 
-  const test = '/test';
-
   useEffect(() => {
     if (thisPost) {
-      import(/* @vite-ignore */ `${MARKDOWNS_PATH}${test}.md`).then((res) => {
-        fetch(res.default)
-          .then((response) => response.text())
-          .then((text) => setContent(text));
-      });
+      // import(/* @vite-ignore */ `${MARKDOWNS_PATH}${test}.md`).then((res) => {
+      //   fetch(res.default)
+      //     .then((response) => response.text())
+      //     .then((text) => setContent(text));
+      // });
+      const markdownPath = `${MARKDOWNS_PATH}/${thisPost.markdown}.md`;
+      fetch(markdownPath)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(
+              `Failed to fetch markdown file: ${response.statusText}`,
+            );
+          }
+          return response.text();
+        })
+        .then((text) => setContent(text))
+        .catch((error) => {
+          console.error(error);
+          setContent('Failed to load content.');
+        });
     }
   }, [thisPost]);
 
